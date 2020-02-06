ae740310f8e37168b6620409e112461ff92ad0a0
paravx2020 <59054523+paravx2020@users.noreply.github.com>
2020-02-06 09:40:09 +0000

    Update hello_world.py
    Date modified

--- hello_world.py
+++ hello_world.py
@@ -13,7 +13,7 @@ def print_hello():
 
 dag = DAG('hello_world', description='Simple tutorial DAG',
           schedule_interval='0 12 * * *',
-          start_date=datetime(2017, 3, 20), catchup=False)
+          start_date=datetime(2020, 2, 6), catchup=False)
 
 dummy_operator = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
 
